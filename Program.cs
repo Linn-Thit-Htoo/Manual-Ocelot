537018a2efca0472be8cc63deb31438e86639666
Linn-Thit-Htoo <lth1212001@gmail.com>
2025-03-18 08:31:59 +0630

    feat: complete gateway including routing, auth and load balancing

--- Program.cs
+++ Program.cs
@@ -1,36 +1,35 @@
 using Manual_Ocelot.Dependencies;
 using Manual_Ocelot.Middlewares;
 
-namespace Manual_Ocelot
+namespace Manual_Ocelot;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddDependencies(builder);
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.AddDependencies(builder);
-
-            var app = builder.Build();
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseMiddleware<GatewayMiddleware>();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseMiddleware<GatewayMiddleware>();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
