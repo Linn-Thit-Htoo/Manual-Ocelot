4f6c497e1c5b779af726308c8fa0cc0491abe1d8
Linn-Thit-Htoo <lth1212001@gmail.com>
2025-03-18 08:30:17 +0630

    -

--- Middlewares/GatewayMiddleware.cs
+++ Middlewares/GatewayMiddleware.cs
@@ -9,116 +9,115 @@ using Manual_Ocelot.Services.TokenValidationServices;
 using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Newtonsoft.Json;
 
-namespace Manual_Ocelot.Middlewares
+namespace Manual_Ocelot.Middlewares;
+
+public class GatewayMiddleware
 {
-    public class GatewayMiddleware
+    private readonly RequestDelegate _next;
+    private readonly Ocelot _ocelot;
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+
+    public GatewayMiddleware(IServiceScopeFactory serviceScopeFactory, RequestDelegate next)
     {
-        private readonly RequestDelegate _next;
-        private readonly Ocelot _ocelot;
-        private readonly IServiceScopeFactory _serviceScopeFactory;
+        string filePath =
+            Path.Combine(Directory.GetCurrentDirectory(), "ocelot.json")
+            ?? throw new Exception("Ocelot JSON file not found.");
+        _serviceScopeFactory = serviceScopeFactory;
 
-        public GatewayMiddleware(IServiceScopeFactory serviceScopeFactory, RequestDelegate next)
+        string jsonStr = File.ReadAllText(filePath);
+        _ocelot = JsonConvert.DeserializeObject<Ocelot>(jsonStr)!;
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext httpContext)
+    {
+        try
         {
-            string filePath =
-                Path.Combine(Directory.GetCurrentDirectory(), "ocelot.json")
-                ?? throw new Exception("Ocelot JSON file not found.");
-            _serviceScopeFactory = serviceScopeFactory;
+            var requestPath = httpContext.Request.Path.ToString();
+            var requestMethod = httpContext.Request.Method;
+            var scope = _serviceScopeFactory.CreateScope();
 
-            string jsonStr = File.ReadAllText(filePath);
-            _ocelot = JsonConvert.DeserializeObject<Ocelot>(jsonStr)!;
-            _next = next;
-        }
+            var route = _ocelot.Routes.FirstOrDefault(r =>
+                requestPath.StartsWith(
+                    r.UpstreamPathTemplate.Replace("{everything}", ""),
+                    StringComparison.OrdinalIgnoreCase
+                ) && r.UpstreamHttpMethod.Contains(requestMethod)
+            );
 
-        public async Task Invoke(HttpContext httpContext)
-        {
-            try
+            if (route is null)
             {
-                var requestPath = httpContext.Request.Path.ToString();
-                var requestMethod = httpContext.Request.Method;
-                var scope = _serviceScopeFactory.CreateScope();
+                httpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+                await httpContext.Response.WriteAsync("Downstream service not found.");
+                return;
+            }
 
-                var route = _ocelot.Routes.FirstOrDefault(r =>
-                    requestPath.StartsWith(
-                        r.UpstreamPathTemplate.Replace("{everything}", ""),
-                        StringComparison.OrdinalIgnoreCase
-                    ) && r.UpstreamHttpMethod.Contains(requestMethod)
-                );
+            string authHeader = httpContext.Request.Headers["Authorization"]!;
+            string[] header_and_token = authHeader.Split(' ');
+            string header = header_and_token[0];
+            string token = header_and_token[1];
 
-                if (route is null)
+            if (route.AuthenticationOptions.AuthenticationProviderKey is not null)
+            {
+                if (authHeader is null || !header.StartsWith("Bearer"))
                 {
-                    httpContext.Response.StatusCode = StatusCodes.Status404NotFound;
-                    await httpContext.Response.WriteAsync("Downstream service not found.");
+                    httpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    return;
+                }
+            }
+
+            if (
+                route.AuthenticationOptions.AllowedScopes is not null
+                && route.AuthenticationOptions.AllowedScopes.Length > 0
+            )
+            {
+                var tokenValidationService =
+                    scope.ServiceProvider.GetRequiredService<ITokenValidationService>();
+
+                var principal = tokenValidationService.ValidateToken(token);
+                if (principal is null)
+                {
+                    httpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
                     return;
                 }
 
-                string authHeader = httpContext.Request.Headers["Authorization"]!;
-                string[] header_and_token = authHeader.Split(' ');
-                string header = header_and_token[0];
-                string token = header_and_token[1];
+                bool hasValidScope = route.AuthenticationOptions.AllowedScopes.All(scope =>
+                    principal.Claims.Any(c => c.Type == "scope" && c.Value == scope)
+                );
 
-                if (route.AuthenticationOptions.AuthenticationProviderKey is not null)
+                if (!hasValidScope)
                 {
-                    if (authHeader is null || !header.StartsWith("Bearer"))
-                    {
-                        httpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                        return;
-                    }
+                    httpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
+                    return;
                 }
-
-                if (
-                    route.AuthenticationOptions.AllowedScopes is not null
-                    && route.AuthenticationOptions.AllowedScopes.Length > 0
-                )
-                {
-                    var tokenValidationService =
-                        scope.ServiceProvider.GetRequiredService<ITokenValidationService>();
-
-                    var principal = tokenValidationService.ValidateToken(token);
-                    if (principal is null)
-                    {
-                        httpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                        return;
-                    }
-
-                    bool hasValidScope = route.AuthenticationOptions.AllowedScopes.All(scope =>
-                        principal.Claims.Any(c => c.Type == "scope" && c.Value == scope)
-                    );
-
-                    if (!hasValidScope)
-                    {
-                        httpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
-                        return;
-                    }
-                }
-
-                var gatewayService = scope.ServiceProvider.GetRequiredService<IGatewayService>();
-
-                HttpResponseMessage response = null!;
-
-                response = route.LoadBalancerOptions.Type switch
-                {
-                    nameof(LoadBalancingConstant.RoundRobin) =>
-                        await gatewayService.ProcessRoundRobinLoadBalancingRequest(
-                            httpContext,
-                            route
-                        ),
-                    nameof(LoadBalancingConstant.LeastConnection) =>
-                        await gatewayService.ProcesssLeastConnectionLoadBalancingRequest(
-                            httpContext,
-                            route
-                        ),
-                    _ => throw new Exception("Invalid Load Balancing Algorithm."),
-                };
-
-                httpContext.Response.StatusCode = (int)response!.StatusCode;
-                string jsonStr = await response.Content.ReadAsStringAsync();
-                await httpContext.Response.WriteAsync(jsonStr);
-                return;
             }
-            catch (Exception ex)
+
+            var gatewayService = scope.ServiceProvider.GetRequiredService<IGatewayService>();
+
+            HttpResponseMessage response = null!;
+
+            response = route.LoadBalancerOptions.Type switch
             {
-                httpContext.Response.StatusCode = (int)HttpStatusCode.BadGateway;
-            }
+                nameof(LoadBalancingConstant.RoundRobin) =>
+                    await gatewayService.ProcessRoundRobinLoadBalancingRequest(
+                        httpContext,
+                        route
+                    ),
+                nameof(LoadBalancingConstant.LeastConnection) =>
+                    await gatewayService.ProcesssLeastConnectionLoadBalancingRequest(
+                        httpContext,
+                        route
+                    ),
+                _ => throw new Exception("Invalid Load Balancing Algorithm."),
+            };
+
+            httpContext.Response.StatusCode = (int)response!.StatusCode;
+            string jsonStr = await response.Content.ReadAsStringAsync();
+            await httpContext.Response.WriteAsync(jsonStr);
+            return;
+        }
+        catch (Exception ex)
+        {
+            httpContext.Response.StatusCode = (int)HttpStatusCode.BadGateway;
         }
     }
 }
