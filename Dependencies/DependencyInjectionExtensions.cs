edd8d5783cea6fdd547de2f4d714906329448393
Linn-Thit-Htoo <lth1212001@gmail.com>
2025-03-18 08:30:08 +0630

    -

--- Dependencies/DependencyInjectionExtensions.cs
+++ Dependencies/DependencyInjectionExtensions.cs
@@ -2,30 +2,29 @@
 using Manual_Ocelot.Services.GatewayServices;
 using Manual_Ocelot.Services.TokenValidationServices;
 
-namespace Manual_Ocelot.Dependencies
+namespace Manual_Ocelot.Dependencies;
+
+public static class DependencyInjectionExtensions
 {
-    public static class DependencyInjectionExtensions
+    public static IServiceCollection AddDependencies(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
     {
-        public static IServiceCollection AddDependencies(
-            this IServiceCollection services,
-            WebApplicationBuilder builder
-        )
-        {
-            builder
-                .Configuration.SetBasePath(builder.Environment.ContentRootPath)
-                .AddJsonFile(
-                    $"appsettings.{builder.Environment.EnvironmentName}.json",
-                    optional: false,
-                    reloadOnChange: true
-                )
-                .AddEnvironmentVariables();
+        builder
+            .Configuration.SetBasePath(builder.Environment.ContentRootPath)
+            .AddJsonFile(
+                $"appsettings.{builder.Environment.EnvironmentName}.json",
+                optional: false,
+                reloadOnChange: true
+            )
+            .AddEnvironmentVariables();
 
-            builder.Services.AddControllers();
-            builder.Services.AddSingleton<IGatewayService, GatewayService>();
-            builder.Services.AddScoped<ITokenValidationService, TokenValidationService>();
-            builder.Services.Configure<AppSetting>(builder.Configuration);
+        builder.Services.AddControllers();
+        builder.Services.AddSingleton<IGatewayService, GatewayService>();
+        builder.Services.AddScoped<ITokenValidationService, TokenValidationService>();
+        builder.Services.Configure<AppSetting>(builder.Configuration);
 
-            return services;
-        }
+        return services;
     }
 }
