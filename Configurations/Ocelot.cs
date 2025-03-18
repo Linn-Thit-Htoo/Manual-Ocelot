19521dd3c37c4e7abba7c70d8dbbbf9f8470a399
Linn-Thit-Htoo <lth1212001@gmail.com>
2025-03-18 08:29:01 +0630

    -

--- Configurations/Ocelot.cs
+++ Configurations/Ocelot.cs
@@ -1,41 +1,40 @@
-﻿namespace Manual_Ocelot.Configurations
+﻿namespace Manual_Ocelot.Configurations;
+
+public class Ocelot
 {
-    public class Ocelot
-    {
-        public Globalconfiguration GlobalConfiguration { get; set; }
-        public List<Route> Routes { get; set; }
-    }
+    public Globalconfiguration GlobalConfiguration { get; set; }
+    public List<Route> Routes { get; set; }
+}
 
-    public class Globalconfiguration
-    {
-        public string BaseUrl { get; set; }
-    }
+public class Globalconfiguration
+{
+    public string BaseUrl { get; set; }
+}
 
-    public class Route
-    {
-        public string UpstreamPathTemplate { get; set; }
-        public string[] UpstreamHttpMethod { get; set; }
-        public List<Downstreamhostandport> DownstreamHostAndPorts { get; set; }
-        public Loadbalanceroptions LoadBalancerOptions { get; set; }
-        public string DownstreamPathTemplate { get; set; }
-        public string DownstreamScheme { get; set; }
-        public Authenticationoptions AuthenticationOptions { get; set; }
-    }
+public class Route
+{
+    public string UpstreamPathTemplate { get; set; }
+    public string[] UpstreamHttpMethod { get; set; }
+    public List<Downstreamhostandport> DownstreamHostAndPorts { get; set; }
+    public Loadbalanceroptions LoadBalancerOptions { get; set; }
+    public string DownstreamPathTemplate { get; set; }
+    public string DownstreamScheme { get; set; }
+    public Authenticationoptions AuthenticationOptions { get; set; }
+}
 
-    public class Loadbalanceroptions
-    {
-        public string Type { get; set; }
-    }
+public class Loadbalanceroptions
+{
+    public string Type { get; set; }
+}
 
-    public class Authenticationoptions
-    {
-        public string AuthenticationProviderKey { get; set; }
-        public string[] AllowedScopes { get; set; }
-    }
+public class Authenticationoptions
+{
+    public string AuthenticationProviderKey { get; set; }
+    public string[] AllowedScopes { get; set; }
+}
 
-    public class Downstreamhostandport
-    {
-        public string Host { get; set; }
-        public int Port { get; set; }
-    }
+public class Downstreamhostandport
+{
+    public string Host { get; set; }
+    public int Port { get; set; }
 }
